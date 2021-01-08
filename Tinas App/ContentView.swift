ec483574a18504ad491c602865cc2d24098e624b
Juhi Joshi <juhiujoshi@gmail.com>
2021-01-08 09:27:33 -0500

    Initial Commit

---
+++ Tinas App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tinas App
+//
+//  Created by New User1 on 1/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tinas App/Tinas_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinas_AppApp.swift
+//  Tinas App
+//
+//  Created by New User1 on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Tinas_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
